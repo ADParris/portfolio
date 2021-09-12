8ad2639035b3e0ec982baf256b82f5e92f6726ce
Andrew Parris <andrew@adparris.com>
2021-09-11 21:24:30 -0700

    disabled loader, may remove at later time

--- src/components/Layout/index.tsx
+++ src/components/Layout/index.tsx
@@ -10,7 +10,7 @@ import { customTheme, Sizes } from '@data/constants';
 import { ISiteMetadataLink } from '@data/models';
 import { setSize, useScreenSizeCheck } from '@utils';
 
-import { Head, Loader } from '@components';
+import { Head } from '@components';
 
 import { Footer } from './Footer';
 import { Header } from './Header';
@@ -33,55 +33,50 @@ export const Layout: React.FC<IComponentProps> = ({
 	socials,
 	title,
 }) => {
-	const [animationComplete, setAnimationComplete] = React.useState(
-		!isHome
-	);
 	const isLargeScreen = useScreenSizeCheck();
-
-	const onAnimationComplete = () => setAnimationComplete(true);
 
 	return (
 		<>
 			<Head description={description} title={title} />
 			<ChakraProvider theme={customTheme}>
 				<ColorModeScript initialColorMode="system" />
-				{!animationComplete ? (
+				{/* {!animationComplete ? (
 					<Loader
 						isLargeScreen={isLargeScreen}
 						onAnimationComplete={onAnimationComplete}
 					/>
-				) : (
+				) : ( */}
+				<Flex
+					as={motion.div}
+					flex={1}
+					justifyContent="center"
+					minH="100vh"
+					mx={isLargeScreen ? 0 : setSize(Sizes.gap)}
+					initial={{ opacity: 0 }}
+					animate={{ opacity: 1 }}
+					exit={{ opacity: 0 }}
+					transition={{ ease: `easeIn` }}
+				>
+					{isHome && isLargeScreen && (
+						<Sides isLargeScreen={isLargeScreen} socials={socials} />
+					)}
 					<Flex
-						as={motion.div}
 						flex={1}
-						justifyContent="center"
-						minH="100vh"
-						mx={isLargeScreen ? 0 : setSize(Sizes.gap)}
-						initial={{ opacity: 0 }}
-						animate={{ opacity: 1 }}
-						exit={{ opacity: 0 }}
-						transition={{ ease: `easeIn` }}
+						flexDir="column"
+						maxW={setSize(Sizes.maxWidth)}
+						w="full"
 					>
-						{isHome && isLargeScreen && (
-							<Sides isLargeScreen={isLargeScreen} socials={socials} />
-						)}
-						<Flex
-							flex={1}
-							flexDir="column"
-							maxW={setSize(Sizes.maxWidth)}
-							w="full"
-						>
-							<Header isLargeScreen={isLargeScreen} />
-							<Main>{children}</Main>
-							{isHome && (
-								<Footer isLargeScreen={isLargeScreen} socials={socials} />
-							)}
-						</Flex>
-						{isHome && isLargeScreen && (
-							<Sides email={email} isLargeScreen={isLargeScreen} />
+						<Header isLargeScreen={isLargeScreen} />
+						<Main>{children}</Main>
+						{isHome && (
+							<Footer isLargeScreen={isLargeScreen} socials={socials} />
 						)}
 					</Flex>
-				)}
+					{isHome && isLargeScreen && (
+						<Sides email={email} isLargeScreen={isLargeScreen} />
+					)}
+				</Flex>
+				{/* )} */}
 			</ChakraProvider>
 		</>
 	);
